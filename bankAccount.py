c4bc908c93e447d443c11574fe0ac6b871bf981e
danieluhr713 <85532956+danieluhr713@users.noreply.github.com>
2021-06-18 15:42:37 -0500

    Update bankAccount.py

--- bankAccount.py
+++ bankAccount.py
@@ -19,9 +19,21 @@ class BankAccount:
         self.acctBalance += self.acctBalance * self.rate
 
 bnkacct1 = BankAccount(0.0345, 150)
+bnkacct2 = BankAccount(0.02355, 1000)
 
 bnkacct1.deposit(150)
+bnkacct1.deposit(200)
+bnkacct1.deposit(300)
+bnkacct1.withdraw(200)
+bnkacct1.withdraw(150)
+bnkacct2.deposit(200)
+bnkacct2.deposit(150)
+bnkacct2.withdraw(130)
+bnkacct2.withdraw(250)
+bnkacct2.withdraw(100)
 bnkacct1.yield_interest()
 bnkacct1.display_account_info()
+bnkacct2.yield_interest()
+bnkacct2.display_account_info()
 
 
